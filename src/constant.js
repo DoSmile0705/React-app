65d2858034b02f022badd9418480402d5a71927a
DevCreator0705 <136826855+DevCreator0705@users.noreply.github.com>
2023-10-08 05:42:41 +0200

    f

--- src/constant.js
+++
@@ -1,8 +0,0 @@
-const LAYOUT_CONST = {
-    VERTICAL_LAYOUT: 'vertical',
-    HORIZONTAL_LAYOUT: 'horizontal',
-    DEFAULT_DRAWER: 'default',
-    MINI_DRAWER: 'mini-drawer'
-};
-
-export default LAYOUT_CONST;
