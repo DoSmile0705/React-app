3081a8e0b1a77d2264f904c4f69f90541ffc907b
DevCreator0705 <136826855+DevCreator0705@users.noreply.github.com>
2023-10-08 06:12:21 +0200

    change text

--- views.py
+++ views.py
@@ -2,10 +2,13 @@ from Cors import App
 from flask import request
 import json
 
-data = []
+
+
+data = ["Hello !!!"]
 
 # modified
 @App.route("/", methods=["GET", "POST"])
+
 def sample_api():
     if request.method == "POST":
         data.append(request.get_json())
