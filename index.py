a653b094f1e86f0d13bb03fd19c055e280d8001a
DevCreator0705 <136826855+DevCreator0705@users.noreply.github.com>
2023-10-08 06:01:24 +0200

    f

--- index.py
+++ index.py
@@ -1,31 +1,8 @@
-from flask import Flask
+import Cors
+import os
+import views
 
+app=Cors.App
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def home():
-    return 'Home Page Route'
-
-
-@app.route('/about')
-def about():
-    return 'About Page Route'
-
-
-@app.route('/portfolio')
-def portfolio():
-    return 'Portfolio Page Route'
-
-
-@app.route('/contact')
-def contact():
-    return 'Contact Page Route'
-
-
-@app.route('/api')
-def api():
-    with open('data.json', mode='r') as my_file:
-        text = my_file.read()
-        return text
+if __name__=="__main__":
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
